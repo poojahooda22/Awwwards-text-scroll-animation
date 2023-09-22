2fd3b3d5903aa3c56d68e965df40784d3861cb90
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-09-22 12:34:21 +0530

    code to animate

--- script.js
+++ script.js
@@ -1,20 +1,85 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+function locomotive() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true ,
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      },
+  
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+  
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+  }
+  locomotive();
 
-var tl =  gsap.timeline({scrollTrigger: {
+
+  function firstPageAnim() {
+    var tl = gsap.timeline();
+  
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.2,
+        ease: Expo.easeInOut,
+    }) 
+        .to(".boundingelem", {
+            y: 0,   
+            duration: 2,
+            delay: -1.4,
+            ease: Expo.easeInOut,
+            stagger: .2
+        })
+        .from(".imagediv", {
+          y: 10,
+          opacity: 0,
+          duration: 1.5,
+          delay: -1,       
+          ease: Expo.easeInOut,
+          stagger: .2
+      })
+  }
+  
+  firstPageAnim();
+
+
+
+
+
+var tl1 =  gsap.timeline({scrollTrigger: {
     trigger: "#part-2",
-    start: "top 80%",
-    end: "bottom 50%",
+    start: "top 50%",
+    end: "bottom 90%",
     // markers: true,
     scrub: 1,
 }})
 
-tl.to(".strip-l", {
+
+
+tl1.to(".strip-l", {
     marginLeft: "0%",
 }, "ac")
 
-tl.to(".strip-r", {
+tl1.to(".strip-r", {
     marginLeft: "-50%",
 }, "ac")
+
